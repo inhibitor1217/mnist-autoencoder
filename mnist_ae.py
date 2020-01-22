ca4e5dd4d3f1f8936df44d2cffe49bb99475135b
inhibitor1217 <cytops1217@gmail.com>
2020-01-22 12:01:40 +0900

    add some efforts :(

--- mnist_ae.py
+++ mnist_ae.py
@@ -2,7 +2,6 @@ import numpy as np
 from tensorflow.keras import Input, Model
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, LeakyReLU
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import multi_gpu_model
 
@@ -31,19 +30,15 @@ def load_mnist_data():
 
 def Encoder(model_name):
     x = _input = Input(shape=(28, 28, 1))
-    x = Conv2D(16, (3, 3), padding='same')(x)
+    x = Conv2D(8, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
-    x = Conv2D(16, (3, 3), padding='same')(x)
+    x = Conv2D(32, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
-    x = LeakyReLU()(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
+    x = Conv2D(64, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
-    x = LeakyReLU()(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
+    x = Conv2D(64, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
 
@@ -53,22 +48,18 @@ def Encoder(model_name):
     return model
 
 def Decoder(model_name):
-    x = _input = Input(shape=(4, 4, 8))
-    x = Conv2D(8, (3, 3), padding='same')(x)
-    x = LeakyReLU()(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
-    x = LeakyReLU()(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
-    x = LeakyReLU()(x)
-    x = Conv2D(8, (3, 3), padding='same')(x)
+    x = _input = Input(shape=(4, 4, 64))
+    x = Conv2D(32, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(16, (3, 3), padding='same')(x)
     x = LeakyReLU()(x)
+    x = UpSampling2D((2, 2))(x)
     x = Conv2D(16, (3, 3))(x)
     x = LeakyReLU()(x)
     x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), padding='same')(x)
+    x = LeakyReLU()(x)
     x = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
     model = Model(_input, x, name=model_name)
@@ -88,32 +79,44 @@ def AE(encoder, decoder, model_name):
 def main():
     (train_x, _), (test_x, _) = load_mnist_data()
 
-    # train_x_noise = train_x + .5 * np.random.normal(0., 1., size=train_x.shape)
-    # test_x_noise  = test_x  + .5 * np.random.normal(0., 1., size=test_x .shape)
+    train_x_noise = np.clip(train_x + .3 * np.random.normal(0., 1., size=train_x.shape), a_min=0, a_max=1)
+    test_x_noise  = np.clip(test_x  + .3 * np.random.normal(0., 1., size=test_x .shape), a_min=0, a_max=1)
 
     encoder = Encoder('')
     decoder = Decoder('')
     autoencoder = AE(encoder, decoder, '')
 
     parallel_model = multi_gpu_model(autoencoder, gpus=2)
-    parallel_model.compile(optimizer=Adam(lr=1e-2), loss='binary_crossentropy')
+    parallel_model.compile(optimizer=Adam(lr=1e-3), loss='binary_crossentropy')
 
-    parallel_model.fit(train_x, train_x, epochs=5, shuffle=True, batch_size=128,
-                    # validation_data=(test_x, test_x),
-                    callbacks=[
-                        TensorBoard(log_dir='./tmp/autoencoder'),
-                        # ModelCheckpoint('./checkpoint/model-{epoch:02d}.h5', save_freq='epoch')
-                    ])
+    parallel_model.fit(train_x_noise, train_x, epochs=30, shuffle=True, batch_size=2048,
+                    validation_data=(test_x_noise, test_x),
+                    # callbacks=[
+                    #     TensorBoard(log_dir='./tmp/autoencoder'),
+                    #     # ModelCheckpoint('./checkpoint/model-{epoch:02d}.h5', save_freq='epoch')
+                    # ]
+    )
 
-    t = test_x[:50]
+    # out = parallel_model.predict(test_x_noise[19:20])
+    # out_img = np.zeros(shape=(28, 28*3))
+    # out_img[:, :28] = test_x_noise[19,:,:,0]
+    # out_img[:, 28:56] = test_x[19,:,:,0]
+    # out_img[:,56:] = out[0,:,:,0]
+    # plt.imshow(out_img)
+    # plt.gray()
+
+    choice = np.random.choice(test_x.shape[0], 100)
+    t = test_x_noise[choice]
+    tt = test_x[choice]
 
     out = parallel_model.predict(t)
 
-    out_img = np.zeros(shape=(280, 280))
-    for i in range(10):
+    out_img = np.zeros(shape=(28*20, 28*15))
+    for i in range(20):
         for j in range(5):
-            out_img[i*28:(i+1)*28, 2*j*28:(2*j+1)*28] = t[5*i+j].reshape(28, 28)
-            out_img[i*28:(i+1)*28, (2*j+1)*28:(2*j+2)*28] = out[5*i+j].reshape(28, 28)
+            out_img[i*28:(i+1)*28, 3*j*28:(3*j+1)*28] = t[5*i+j].reshape(28, 28)
+            out_img[i*28:(i+1)*28, (3*j+1)*28:(3*j+2)*28] = tt[5*i+j].reshape(28, 28)
+            out_img[i*28:(i+1)*28, (3*j+2)*28:(3*j+3)*28] = out[5*i+j].reshape(28, 28)
 
     plt.imshow(out_img)
     plt.gray()
